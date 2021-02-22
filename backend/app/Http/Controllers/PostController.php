bb861656ccf8f23667832d427f5f29d996856a1a
takashimayuusuke <taka.z34.vq37@gmail.com>
2021-02-23 08:12:32 +0900

    Postコントローラー修正

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
 class PostController extends Controller
 {
     /**
